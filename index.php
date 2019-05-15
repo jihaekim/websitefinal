d3ddae5b696d64758c2b4e1743df5842ea692f4c
Hyunrak Kang <DB@HyunraksMacBook.home>
2019-05-14 22:18:06 -0400

    vv

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include_once('home.html') ?>
+<?php include_once('appointment.html') ?>
+
+<?php include_once('policy.html') ?>
+<?php include_once('tribeca.html') ?>
+<?php include_once('uws.html') ?>
