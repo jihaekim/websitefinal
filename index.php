6220083f7f1bfea2d2e9bcb25bf327e8cfa68294
Hyunrak Kang <DB@HyunraksMacBook.home>
2019-05-15 23:37:29 -0400

    git

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php include_once('home.html') ?>
-<?php include_once('appointment.html') ?>
-
-<?php include_once('policy.html') ?>
-<?php include_once('tribeca.html') ?>
-<?php include_once('uws.html') ?>
 
 
-<?php
-    header('Location: home.html');
-
-?>
